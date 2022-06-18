38f5aa564a928c9baa6f684f8bde1f48b6d0e9dd
Raghu K <raghuk.vit@gmail.com>
2022-06-18 08:21:48 +0530

    Update roboshop-infra/frontend-infra/

--- main.tf
+++ main.tf
@@ -20,7 +20,7 @@ module "app" {
   NODE_TYPE               = var.EC2_NODE_TYPE
   PORT                    = var.PORT
   WORKSTATION_IP          = var.WORKSTATION_IP
-  VPC_ACCESS_TO_ALB       = [module.vpc.VPC_CIDR, "172.31.0.0/16", "10.10.5.0/24"]
+  VPC_ACCESS_TO_ALB       = [module.vpc.VPC_CIDR, "172.31.0.0/16", "10.10.5.0/24", "${module.vpc.NGW_PRIVATE_IP}/32"]
   PRIVATE_HOSTED_ZONE_ID  = module.vpc.PRIVATE_HOSTED_ZONE_ID
 }
 
